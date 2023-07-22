4eb0a692010d5aafd4dfbb8761681730a46f70ad
c20kyo1827 <33478517+c20kyo1827@users.noreply.github.com>
2023-07-23 01:45:49 +0800

    stage1 week3 #1 4 task

---
+++ week3/week3.js
@@ -0,0 +1,147 @@
+// Week1 advanced
+// Add the drop down function to the hamburger icon
+
+// When click the icon => add the show class into icon"s class list
+function clickToAddShow(){
+    const eDropDown = document.getElementsByClassName("drop_down");
+    for (let i = 0; i < eDropDown.length; i++) {
+        eDropDown.item(i).classList.add("show");
+    }
+}
+
+
+// When click on the region outside the drop down list => remove the show class from icon"s class list
+function clickToRemoveShow(){
+    window.onclick = function(event) {
+        // console.log(event.target);
+        if (!event.target.matches(".menu_text") && 
+            !event.target.matches(".menu_text_item") &&
+            !event.target.matches(".size_figure") && 
+            !event.target.matches(".menu_figure")
+        )
+        {
+            // console.log("Not");
+            const eDropDown = document.getElementsByClassName("drop_down");
+            for (let i = 0; i < eDropDown.length; i++) {
+                if (eDropDown.item(i).classList.contains("show")) {
+                    // console.log("remove");
+                    eDropDown.item(i).classList.remove("show");
+                }
+            }
+        }
+    }
+}
+
+// Week3
+// fetch images and append these on the block
+let titleList = []
+let imgUrlList = []
+let globalIter = 0;
+const numPromot = 3;
+const eachRowTitle = 12;
+function preFetchAndAppendImages(){
+    let url = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json";
+    // Get the image url from the url json
+
+    // Use create element to create img and append img to the element => document.createElement() and appendChild()
+    // Should keep the original layout format
+
+    // Fetch the url and get the response
+    // Use response to get json format dataJSON
+    // Process
+    
+    console.log("Initial");
+    fetch(url)
+        .then((response) => {
+            console.log("Response");
+            return response.json()
+        })
+        .then(
+            (dataJSON) => {
+                console.log("Parse json");
+                let attractions = dataJSON["result"]["results"];
+                for(const val of attractions){
+                    let title = val["stitle"];
+                    let imgUrl = String(val["file"]).toLowerCase().split(".jpg")[0] + ".jpg";
+                    titleList.push(title)
+                    imgUrlList.push(imgUrl);
+                }
+
+                // Append Promotion 1~3
+                // [0, 3)
+                const eRow1ImgSeting = document.getElementsByClassName("figure_row1_img_setting");
+                const eRow1Prmt = document.getElementsByClassName("figure_row1_prmt");
+                for(globalIter=0 ; globalIter<numPromot ; globalIter++){
+                    // Image src
+                    let img = document.createElement("img");
+                    img.src = imgUrlList[globalIter];
+                    img.width = 80;
+                    img.height = 50;
+                    eRow1ImgSeting.item(globalIter).appendChild(img);
+                    // Image title
+                    let div = document.createElement("div");
+                    div.textContent = titleList[globalIter];
+                    eRow1Prmt.item(globalIter).appendChild(div);
+                }
+                // Append Title for first 12
+                // [3, 3+12)
+                const eRow2Title = document.getElementsByClassName("figure_row2_title");
+                for( ; globalIter<numPromot+eachRowTitle ; globalIter++){
+                    // Image src
+                    let divImgSetting = document.createElement("div");
+                    divImgSetting.classList.add("figure_row2_img_setting");
+                    let img = document.createElement("img");
+                    img.src = imgUrlList[globalIter];
+                    img.setAttribute("width", "100%");
+                    img.setAttribute("height", "100%");
+                    // img.setAttribute("object-fit", "contain");
+                    divImgSetting.appendChild(img);
+                    eRow2Title.item(globalIter-numPromot).appendChild(divImgSetting);
+                    // Image title
+                    let divTitle = document.createElement("div");
+                    divTitle.classList.add("figure_row2_txt");
+                    divTitle.textContent = titleList[globalIter];
+                    eRow2Title.item(globalIter-numPromot).appendChild(divTitle);
+                }
+            }
+        )
+    console.log("End");
+    for(let i=0 ; i<titleList.length ; i++){
+        console.log(titleList[i]);
+    }
+}
+
+async function loadMoreImg(){
+    const eRow2 = document.getElementsByClassName("figure_row2");
+    for(let i=0 ; i<eachRowTitle ; i++){
+        if(i+globalIter >= imgUrlList.length){
+            globalIter += i;
+            break;
+        }
+        // Create figure row2 title
+        let divRow2Title = document.createElement("div");
+        divRow2Title.classList.add("figure_row2_title");
+        // Image src
+        let divImgSetting = document.createElement("div");
+        divImgSetting.classList.add("figure_row2_img_setting");
+        let img = document.createElement("img");
+        img.src = imgUrlList[i+globalIter];
+        img.setAttribute("width", "100%");
+        img.setAttribute("height", "100%");
+        // img.setAttribute("object-fit", "contain");
+        divImgSetting.appendChild(img);
+        divRow2Title.appendChild(divImgSetting);
+        // Image title
+        let divTitle = document.createElement("div");
+        divTitle.classList.add("figure_row2_txt");
+        divTitle.textContent = titleList[i+globalIter];
+        divRow2Title.appendChild(divTitle);
+        // Append to the figure row2
+        eRow2.item(0).appendChild(divRow2Title);
+    }
+    globalIter += eachRowTitle;
+    console.log(globalIter);
+}
+
+clickToRemoveShow();
+preFetchAndAppendImages();
---
+++ week3/week3.py
@@ -0,0 +1,112 @@
+import requests
+import json
+import csv
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import sys
+
+def task1():
+    """
+        Use the specific url to convert the .json format to the .csv format
+        Don't use any third party tools
+    """
+    url = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
+    data_json = json.loads(urlopen(url).read())
+    # ====================================
+    # For checking the json file content
+    # ====================================
+    # stitle => attraction name
+    # address => use address to extract the district of Taiwan
+    # longitude, latitude
+    # file => get the first img html
+    # with open("test.txt", "w", encoding="UTF-8") as f:
+    #     for list in data_json["result"]["results"]:
+    #         for key,val in list.items():
+    #             f.write(str(key) + "\n")
+    #             f.write("    " + str(val) + "\n")
+    #         f.write("=="*20 + "\n")
+
+    # For the district
+    dist_set = ["中正區","萬華區","中山區","大同區","大安區","松山區","信義區","士林區","文山區","北投區","內湖區","南港區"]
+    # For the MRT
+    mrt_stop_dict = {}
+    with open("attraction.csv", "w", encoding="utf-8-sig", newline='') as atrc_file, open("mrt.csv", "w", encoding="utf-8-sig", newline='') as mrt_file:
+        atrc_writer = csv.writer(atrc_file)
+        mrt_writer = csv.writer(mrt_file)
+        for list in data_json["result"]["results"]:
+            # ============
+            # Attraction
+            # ============
+            atrc_row = []
+            # Attraction name
+            atrc_row.append(list["stitle"])
+            # District name
+            for dist in dist_set:
+                word = list["address"].encode('utf8')[3:].decode('utf8')
+                if word.find(dist) != -1:
+                    atrc_row.append(dist)
+            # Longitude
+            atrc_row.append(list["longitude"])
+            # Latitude
+            atrc_row.append(list["latitude"])
+            # First jpg html
+            atrc_row.append(list["file"].lower().split(".jpg")[0]+".jpg")
+            # Write to CSV
+            atrc_writer.writerow(atrc_row)
+
+            # ===========
+            # MRT parse
+            # ===========
+            if list["MRT"] == None: continue
+            if list["MRT"] not in mrt_stop_dict:
+                mrt_stop_dict[list["MRT"]] = [list["stitle"]]
+            else:
+                mrt_stop_dict[list["MRT"]].append(list["stitle"])
+        # ============
+        # MRT write
+        # ============
+        for key,val in mrt_stop_dict.items():
+            mrt_writer.writerow([key] + val)
+
+def task2():
+    """
+        Use BeautifulSounp4 to parse html content
+        => 3 pages
+        title, # of tweets, release time
+    """
+    url_prefix = "https://www.ptt.cc"
+    url = "https://www.ptt.cc/bbs/movie/index.html"
+    num_pages_catched = 3
+
+    with open("movie.txt", "w", encoding="utf-8-sig", newline='') as movie_file:
+        for i in range(0,num_pages_catched):
+            # Get the current page soup
+            soup = BeautifulSoup(requests.get(url).text, "html.parser")
+
+            # Parse title and # of tweets
+            title = soup.select("div.title")
+            tweets = soup.select("div.nrec")
+            # Change to the page and get the time
+            sub_pages = soup.select("div.title > a")
+            for iter,sub_page in enumerate(sub_pages):
+                if "本文已被刪除" in title[iter].text : continue
+                tweet_page_href = sub_page["href"]
+                tweet_url = url_prefix + tweet_page_href
+                soup = BeautifulSoup(requests.get(tweet_url).text, "html.parser")
+                meta_value = soup.select("div.article-metaline>span.article-meta-value")
+                movie_file.write("".join(title[iter].text.split("\n")) + ",")
+                movie_file.write("".join(tweets[iter].text.split("\n")) + ",")
+                movie_file.write("".join(meta_value[-1].text.split("\n")) + "\n")
+
+
+            # Change to the previous page
+            soup = BeautifulSoup(requests.get(url).text, "html.parser")
+            btn = soup.select("div.btn-group > a")
+            up_page_href = btn[3]["href"]
+            url = url_prefix + up_page_href
+
+
+
+if __name__=="__main__":
+    # task1()
+    task2()
