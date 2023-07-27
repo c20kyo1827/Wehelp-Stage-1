511b6d8e73b3c7b73ba54353fe29152508fd8925
c20kyo1827 <33478517+c20kyo1827@users.noreply.github.com>
2023-07-27 10:17:13 +0800

    stage week4 #1

---
+++ week4/static/week4.js
@@ -0,0 +1,6 @@
+// Check whether the checkedbox is checked before submitting
+function isChecked(){
+    if (document.getElementById("checkbox").checked  == false) {
+        alert("Please check the ckeckbox first");
+    } 
+}
