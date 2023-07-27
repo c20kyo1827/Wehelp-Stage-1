715eb9ce25fa909616f75f57d79f4daea15a13d7
c20kyo1827 <33478517+c20kyo1827@users.noreply.github.com>
2023-07-27 13:13:02 +0800

    stage1 week4 #2

--- week4/app.py
+++ week4/app.py
@@ -8,15 +8,25 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = b"_5#y2LF4Q8z\n\xec]/"
 # app.config["PERMANENT_SESSION_LIFETIME"] = False
 
+# Main page
 @app.route("/", endpoint="index", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        if request.form.get("checkbox") == "on":
+        if request.form.get("checkbox") == "on" and request.form["calculate"] == "login":
             session["account"] = request.form["account"]
             session["password"] = request.form["password"]
-            return redirect(url_for("signin"), code=307) # PO
+            return redirect(url_for("signin"), code=307)
+        elif request.form["calculate"] == "calculate":
+            if request.form["integer"].isdecimal() == True and int(request.form["integer"]) > 0:
+                return redirect(url_for("sqaureNum", number=request.form["integer"]))
     return render_template("index.html")
 
+@app.route("/sqaure/<int:number>", endpoint="sqaureNum")
+def sqaureNum(number):
+    sqaureResult = number**2
+    return render_template("number.html", sqaureResult=sqaureResult)
+
+# Sigin endpoint
 @app.route("/signin", endpoint="signin", methods=["POST"])
 def signin():
     print("Signin")
@@ -36,11 +46,14 @@ def signin():
     else:
         return redirect(url_for("errorLogin", message="Username or password is not correct"))
 
+# Signout endpoint
 @app.route("/signout", endpoint="signout" , methods=["GET"])
 def signout():
     session["state"] = False
     return redirect(url_for("index"))
 
+# Login Page
+# Succcuss
 @app.route("/member", endpoint="successLogin")
 def successLogin():
     # Check sign-in state
@@ -48,7 +61,7 @@ def successLogin():
         return redirect(url_for("index"))
     else:
         return render_template("member.html")
-
+# Fail
 @app.route("/error", endpoint="errorLogin")
 def errorLogin():
     msg_content = request.args.get("message", "Username or password is not correct")
--- week4/static/week4.js
+++ week4/static/week4.js
@@ -4,3 +4,14 @@ function isChecked(){
         alert("Please check the ckeckbox first");
     } 
 }
+
+// Check whether the number is positve integer
+function isPositiveNum(){
+    let number = Number(document.getElementById("integer").value);
+    if (!Number.isInteger(number)) {
+        alert("Please enter a positive number");
+    }
+    else if (number <= 0) {
+        alert("Please enter a positive number");
+    }
+}
